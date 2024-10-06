6c32306e77c34130d6d671aab01cdc2ef077e89a
JATisha1 <jarintishaqa@gmail.com>
2024-10-06 10:16:24 +0530

    Added a conditional statement to skip lines with more than one word.

--- ex-08.py
+++ ex-08.py
@@ -5,6 +5,9 @@ for line in han:
     print('Line:', line)
     wds = line.split()
     print('Words:' ,wds)
+    #Guardian
+    if len(wds) > 1 :
+        continue
     if wds [0] != 'From' :
         print ('Ignore')
         continue
