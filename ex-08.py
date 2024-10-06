96bbfe0748390fe3b9aced2cc51c43348048a417
JATisha1 <jarintishaqa@gmail.com>
2024-10-06 10:18:26 +0530

    "Removed 2 lines, added 2 lines, and modified 1 line in ex-08.py"

--- ex-08.py
+++ ex-08.py
@@ -3,11 +3,10 @@ han = open('mbox-short.txt')
 for line in han:
     line = line.rstrip()
     print('Line:', line)
+    if line == '' :
+        print('Skip Blank')
     wds = line.split()
     print('Words:' ,wds)
-    #Guardian
-    if len(wds) > 1 :
-        continue
     if wds [0] != 'From' :
         print ('Ignore')
         continue
