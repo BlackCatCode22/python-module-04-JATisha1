4dfc55b90923e4f229d0231739c4f1e27e682df1
JATisha1 <jarintishaqa@gmail.com>
2024-10-06 10:22:13 +0530

    #guardian in a compound statement

--- ex-08.py
+++ ex-08.py
@@ -3,11 +3,12 @@ han = open('mbox-short.txt')
 for line in han:
     line = line.rstrip()
     print('Line:', line)
-    if line == '' :
-        print('Skip Blank')
     wds = line.split()
     print('Words:' ,wds)
+    #guardian in a compound statement
+    if len(wds) > 3 or wds[0] != 'From':
+        continue
     if wds [0] != 'From' :
-        print ('Ignore')
+       # print ('Ignore')
         continue
     print(wds[2])  
