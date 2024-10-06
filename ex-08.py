292301c8e6237955e3649f964a8590a9ed46e261
JATisha1 <jarintishaqa@gmail.com>
2024-10-06 10:14:48 +0530

    Added print statements to debug line and word processing in mbox-short.txt file reader.

--- ex-08.py
+++ ex-08.py
@@ -2,7 +2,10 @@ han = open('mbox-short.txt')
 
 for line in han:
     line = line.rstrip()
+    print('Line:', line)
     wds = line.split()
+    print('Words:' ,wds)
     if wds [0] != 'From' :
+        print ('Ignore')
         continue
     print(wds[2])  
