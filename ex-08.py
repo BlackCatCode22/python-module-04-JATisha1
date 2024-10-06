c3986304dd6a6fa1f674e29705aa2d874ba5cbf8
JATisha1 <jarintishaqa@gmail.com>
2024-10-06 10:24:45 +0530

    "Refactored conditional statements in loop to simplify logic"

--- ex-08.py
+++ ex-08.py
@@ -6,9 +6,8 @@ for line in han:
     wds = line.split()
     print('Words:' ,wds)
     #guardian in a compound statement
-    if len(wds) > 3 or wds[0] != 'From':
-        continue
-    if wds [0] != 'From' :
+    
+    if len(wds) < 3  or wds [0] != 'From' :
        # print ('Ignore')
-        continue
+     continue
     print(wds[2])  
